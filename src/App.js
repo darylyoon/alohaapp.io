cecdd11ac1a695ef8d18387c13a3da265beead2f
daniellaiex <88719093+daniellaiex@users.noreply.github.com>
2023-03-09 15:54:11 +0800

    navbar routing

--- src/App.js
+++ src/App.js
@@ -8,11 +8,17 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 import Pdp from './pages/pdp';
 import Home from './pages/home';
 import Checkout from './pages/checkout';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/browse" element={<BrowsePage/>}/>
+        <Route path="/pdp" element={<Pdp/>}/>
+      </Routes>
       {/* <Pdp/> */}
       {/* <Home/> */}
       <Checkout/>
